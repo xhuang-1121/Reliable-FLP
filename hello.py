2278696b430252e95a0c4d624e376c60a0e18b05
HanZhang-ZERO <zhanghan347@gmail.com>
2019-10-17 23:45:41 +0800

    hi

--- hello.py
+++ hello.py
@@ -5,11 +5,13 @@ def test2():
     print('haha')
     a = [1, 2, 3, 4]
     a[0] = 9
-    a[2] = 3
+    a[2] = 312
+    for i in range(5):
+        print(i)
 
 
 def test3():
     print("test")
-    
+
 
 test2()
