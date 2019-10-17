fb24e9abe1b3fea599d0694f7d2e56cea264b781
HanZhang-ZERO <zhanghan347@gmail.com>
2019-10-17 23:23:01 +0800

    random

--- hello.py
+++ hello.py
@@ -4,14 +4,12 @@ print('hello, VS code')
 def test2():
     print('haha')
     a = [1, 2, 3, 4]
-    a[0] = 2
+    a[0] = 9
+    a[2] = 3
 
 
 def test3():
     print("test")
-    print('iwow')
-    print('hello')
-    print('hi')
-
+    
 
 test2()
