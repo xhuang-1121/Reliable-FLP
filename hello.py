b19c6ea0d02883b906b944b20497d3563df6cff6
HanZhang-ZERO <zhanghan347@gmail.com>
2019-10-17 23:08:29 +0800

    wow

--- hello.py
+++ hello.py
@@ -4,11 +4,14 @@ print('hello, VS code')
 def test2():
     print('haha')
     a = [1, 2, 3, 4]
+    a[0] = 2
 
 
 def test3():
     print("test")
-    print('it is me')
+    print('iwow')
+    print('hello')
+    print('hi')
 
 
 test2()
