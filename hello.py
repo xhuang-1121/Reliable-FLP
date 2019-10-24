2b8da6a6731ca0de78940ea3276dfcd83c5bb472
HanZhang-ZERO <56686001+HanZhang-ZERO@users.noreply.github.com>
2019-10-24 17:07:42 +0800

    update

--- hello.py
+++ hello.py
@@ -8,8 +8,15 @@ def test2():
     a[2] = 312
     for i in range(5):
         print(i)
+    return a[2]
 
 
 def test3():
-    print("test")
-    print("coding is cool!")
+    b = "test"
+    c = "This is cool!"
+    print(b)
+    print(c)
+    return c
+
+tem1 = test2()
+tem2 = test3()
